335a5dc0aec72578ec540668a52363b9b095ff2a
edwin <edwingrisalescalle@gmail.com>
2022-05-11 01:20:52 +0000

    Solución ejercicio 2

--- Ejercicio2.sh
+++ Ejercicio2.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+trap ctrl_c INT
+function ctrl_c(){
+exit 0
+}
+
 # Cuales son los usuarios que tienen bin bash como consola
+
+awk -F':' '{ if($7=="/bin/bash" ){  print "Usuario: " $1 " Consola: "  $7}}' /etc/passwd
