b2a422cc24a7a3c159563149bfdc60badc8b3904
edwin <edwingrisalescalle@gmail.com>
2022-05-11 00:47:58 +0000

    Solución ejercicio 1

--- Ejercicio1.sh
+++ Ejercicio1.sh
@@ -1,2 +1,15 @@
+#! /bin/bash
+trap ctrl_c INT
+
+function ctrl_c(){
+exit 0
+}
+
+
 #Crear un Script que muestre cuales usuarios tienen un User id mayor a 1000
+
+ getent passwd | awk -F: '{ if($3>1000) {print "UID > 1000: "  $3 }}'
+
+
+#echo "User Id $UID"
 
