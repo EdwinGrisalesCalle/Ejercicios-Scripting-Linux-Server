1e84bc907cbfcfd0a3216c957a1324f92c2f6e63
edwin <edwingrisalescalle@gmail.com>
2022-05-12 02:03:28 +0000

    Solución ejercicio 4

--- Ejercicio4.sh
+++ Ejercicio4.sh
@@ -1,2 +1,26 @@
+#!/bin/bash
+trap ctrl_c INT
+function ctrl_c(){
+exit 0
+}
+
 # Recibir la url de un sitio web y decir si ese sitio esta bien o tiene
 # algun tipo de problema.
+
+if [[ $#>0  ]]
+then
+status=$(curl -s -o /dev/null -w "%{http_code}" $1)
+
+	if [[ $status == 200 ]]	
+	then
+		echo "La pagina funciona correctamente [[status $status]]"
+	else
+		echo "La pagina presenta problemas [[status $status]]"		
+	fi
+
+else
+echo "Debe ingresar una url como parametro para ejecutar este script"
+exit 1
+
+fi
+
